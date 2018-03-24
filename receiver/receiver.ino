62880af02b2c6b760362f9f3833aabf067d708e3
MacBook Pro <macbookpro@My-MacBook-Pro.local>
2018-03-24 19:49:41 -0400

    Added led statements

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -7,24 +7,35 @@
 * Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
 */
 
-// Hello
-
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+
 RF24 radio(7, 8); // CE, CSN
 const byte address[6] = "00001";
+
 void setup() {
   Serial.begin(9600);
   radio.begin();
   radio.openReadingPipe(0, address);
   radio.setPALevel(RF24_PA_MIN);
   radio.startListening();
+  pinMode(10,OUTPUT);
 }
 void loop() {
   if (radio.available()) {
     char text[32] = "";
     radio.read(&text, sizeof(text));
+      if(text == "1")
+    {
+        digitalWrite(10, HIGH);
+    }
+    if(text == "0")
+    {
+        digitalWrite(10, LOW);
+    }
+  
+    
     Serial.println(text);
   }
 }
